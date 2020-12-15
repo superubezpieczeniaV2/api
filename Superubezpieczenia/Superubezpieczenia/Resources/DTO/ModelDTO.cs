7d0c8302ebfdeddea58fb39524d04d2159c2d8e4
szczepanrogaczewski <56364917+szczepanrogaczewski@users.noreply.github.com>
2020-12-15 18:09:21 +0100

    Add files via upload

---
+++ Superubezpieczenia/Superubezpieczenia/Resources/DTO/ModelDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superubezpieczenia.Resources.DTO
+{
+    public class ModelDTO
+    {
+        public string Name { get; set; }//czy tylko tyle?
+    }
+}
