630c1b5f23690ec69abe9837f3dc544262d4272a
szczepanrogaczewski <56364917+szczepanrogaczewski@users.noreply.github.com>
2020-12-15 17:54:56 +0100

    Add files via upload

---
+++ Superubezpieczenia/Superubezpieczenia/Controllers/ModelController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Superubezpieczenia.Domain.Models;
+using Superubezpieczenia.Domain.Services;
+using Superubezpieczenia.Resources.DTO;
+using Superubezpieczenia.Resources.ViewModels;
+
+namespace Superubezpieczenia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelController : ControllerBase
+    {
+        public readonly IModelService _modelService;
+        public readonly IMapper _mapper;
+
+        public ModelController(IModelService modelService, IMapper mapper)
+        {
+            _modelService = modelService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Model>> AllModels()
+        {
+            var model = await _modelService.AllModels();
+            return model;
+        }
+        [HttpPost]
+        public ActionResult<ModelVM> AddModel(ModelDTO modelDTO)
+        {
+            var model = _mapper.Map<Model>(modelDTO);
+            _modelService.AddModel(model);
+            _modelService.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteModel(int id)
+        {
+            var dModel = _modelService.FindById(id);
+            if (dModel==null)
+            {
+                return NotFound();
+            }
+            _modelService.DeleteModel(dModel);
+            _modelService.SaveChanges();
+            return Ok();
+        }
+        public ActionResult UpdateModel(ModelDTO modelDTO, int id)
+        {
+            var uMark = _modelService.FindById(id);
+            if (uMark == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(modelDTO, uMark);
+            _modelService.UpdateModel(uMark);
+            _modelService.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
+
