629ea8fb39dc1307f1253be9956e86cea418c54c
szczepanrogaczewski <56364917+szczepanrogaczewski@users.noreply.github.com>
2020-12-15 17:55:45 +0100

    Add files via upload

---
+++ Superubezpieczenia/Superubezpieczenia/Domain/Repositories/IModelRepository.cs
@@ -0,0 +1,20 @@
+﻿using Superubezpieczenia.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superubezpieczenia.Domain.Repositories
+{
+  public  interface IModelRepository
+    {
+        Task<IEnumerable<Model>> AllModels();
+        Model SelectedModel(string name);
+        Model FindById(int id);
+        void AddModel(Model model);
+        void DeleteModel(Model model);
+        void UpdateModel(Model model);
+
+        bool SaveChanges();
+    }
+}
---
+++ Superubezpieczenia/Superubezpieczenia/Domain/Services/IModelService.cs
@@ -0,0 +1,20 @@
+﻿using Superubezpieczenia.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superubezpieczenia.Domain.Services
+{
+  public  interface IModelService
+    {
+        Task<IEnumerable<Model>> AllModels();
+        Model SelectedModel(string name);
+        Model FindById(int id);
+        void AddModel(Model model);
+        void DeleteModel(Model model);
+        void UpdateModel(Model model);
+
+        bool SaveChanges();
+    }
+}
