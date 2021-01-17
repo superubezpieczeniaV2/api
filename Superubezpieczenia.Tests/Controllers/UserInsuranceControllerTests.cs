c2e67356f128c7e5c3cb54bb029da9d197fbab56
Anna Kaczmarczyk <ania.natalia.kaczmarczyk@gmail.com>
2021-01-17 19:51:35 +0100

    Add 'UserInsuranceControllerTests'

---
+++ Superubezpieczenia.Tests/Controllers/UserInsuranceControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Superubezpieczenia.Controllers;
+using Superubezpieczenia.Domain.Models;
+using Superubezpieczenia.Domain.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Superubezpieczenia.Tests.Controllers
+{
+    [TestClass]
+    public class UserInsuranceControllerTests
+    {
+        public IUserInsuranceService _userInsuranceService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _userInsuranceService = Substitute.For<IUserInsuranceService>();
+            _userInsuranceService.FindUserInsurance(Arg.Is<string>(x => x == "User1"))
+                .Returns(Task.FromResult((IEnumerable<Insurance>)new List<Insurance>()
+                {
+                    new Insurance { Id = "1" }
+                })
+            );
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void FindUserInsurance_ExistingUserInsurance_InsuranceReturned()
+        {
+            //Arrange  
+            var user = "User1";
+            var controller = new UserInsuranceController(_userInsuranceService, null);
+
+            //Act
+            var userInsurance = controller.FindUserInsurance(user).Result;
+
+            //Asserts
+            userInsurance.Should().ContainSingle(x => x.Id == "1");
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void FindUserInsurance_NonexistentUserInsurance_NothingReturned()
+        {
+            //Arrange  
+            var user = "User2";
+            var controller = new UserInsuranceController(_userInsuranceService, null);
+
+            //Act
+            var userInsurance = controller.FindUserInsurance(user).Result;
+
+            //Asserts
+            userInsurance.Should().BeEmpty();
+        }
+    }
+}
